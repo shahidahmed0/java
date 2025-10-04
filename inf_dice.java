adb8a430f09aa0dcf6e1ccd40d41830fbb6226dd
shahidahmed0 <shahidahmed43472@gmail.com>
2025-10-04 16:22:40 +0400

    Add files via upload
    rolling a dice with loop

---
+++ inf_dice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+public class inf_dice {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Random ran= new Random();
+		int dice= ran.nextInt(1,7);
+
+		while (1==1) {
+			System.out.println("to roll the dice write \'roll\' ");
+			String r=input.nextLine().toLowerCase();
+			System.out.println((r.equals("roll")?dice:"incorrect,try again"));
+
+		}
+	}
+}
